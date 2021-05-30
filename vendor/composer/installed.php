49277cdf0a9ba88bd1d79cbd6c3c719b81d0c348
Agus Tri Haryono <agustharyono24@gmail.com>
2021-05-31 05:23:09 +0800

    first init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v0.5.1',
+    'version' => '0.5.1.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'v0.5.1',
+      'version' => '0.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'codeigniter/framework' => 
+    array (
+      'pretty_version' => '3.1.11',
+      'version' => '3.1.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b73eb19aed66190c10c9cad476da7c36c271d6dc',
+    ),
+    'mikey179/vfsstream' => 
+    array (
+      'pretty_version' => 'v1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc0fe8f4d0b527254a2dc45f0c265567c881d07e',
+    ),
+  ),
+);
